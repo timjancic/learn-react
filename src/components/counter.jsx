1f3500e5715719258873741851605fc0b59fe703
timjancic <timjancic.gravity@gmail.com>
2022-02-18 15:34:14 -0600

    Basic changes to start making shopping cart counter

---
+++ src/components/counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 ";
+    classes += (this.state.count === 0) ? "bg-warning" : "bg-primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
