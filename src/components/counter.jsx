62b0cf93a315d0b4deea5d38bfae8d085b3a1bce
timjancic <timjancic.gravity@gmail.com>
2022-02-21 16:20:01 -0600

    Added handle increment function on click

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  // constructor() {
+  //   super(); //need to refer to the base object
+  //   this.handleIncrement = this.handleIncrement.bind(this); //Need to bind this function to the object so that we can pass handleIncrement as an argument?
+  // }
+
+  //Using an arrow function does not require binding
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
 
   render() {
 
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <button
+        onClick={this.handleIncrement}
+        className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>) }
+        </ul>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 ";
-    classes += (this.state.count === 0) ? "bg-warning" : "bg-primary";
+    classes += (this.state.count === 0) ? "bg-warning text-dark" : "bg-primary";
     return classes;
   }
 
