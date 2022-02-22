bcf0aad46ef743739766edb42b84e6d108137eb4
timjancic <timjancic.gravity@gmail.com>
2022-02-22 13:43:27 -0600

    Raised state to app.js and made some cosmetic changes

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          NavBar{" "}
+          <span className="badge rounded-pill bg-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+
+}
+
+export default NavBar;
